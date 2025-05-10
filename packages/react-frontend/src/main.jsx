8768eb9b34cefae1983454f4a3956b2c582747db
ThomHags611 <thomashagos611@gmail.com>
2025-05-10 15:09:53 -0700

    style: removed comment

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import "./App.css";
-import AppRouter from "./routes/Router.jsx"; // ✅ this points to your Router.jsx file
+import AppRouter from "./routes/Router.jsx"; 
 import { GameProvider } from "./context/GameContext.jsx";
 
 const root = createRoot(document.getElementById("root"));
