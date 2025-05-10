8cda7db54404e2702f27921ebc2f5435cac39ba1
ThomHags611 <thomashagos611@gmail.com>
2025-05-10 14:29:37 -0700

    fix: all routing is working, uncomment imports once pages are implemented

---
+++ packages/react-frontend/src/context/GameContext.jsx
@@ -0,0 +1,20 @@
+// src/context/GameContext.jsx
+import React, { createContext, useState, useContext } from "react";
+
+const GameContext = createContext();
+
+export function GameProvider({ children }) {
+  const [score, setScore] = useState(0);
+  const [highscore, setHighscore] = useState(0);
+  const [gameStatus, setGameStatus] = useState("not-started"); // or "playing", "lost", etc.
+
+  return (
+    <GameContext.Provider value={{ score, setScore, highscore, setHighscore, gameStatus, setGameStatus }}>
+      {children}
+    </GameContext.Provider>
+  );
+}
+
+export function useGame() {
+  return useContext(GameContext);
+}
