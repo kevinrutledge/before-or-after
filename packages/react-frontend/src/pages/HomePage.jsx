474688fa884b0a86e8947632230d53764cb03caf
Shonueld <seangriffindev@gmail.com>
2025-05-11 00:00:15 -0700

    feat: home page finished and styled

--- packages/react-frontend/src/pages/HomePage.jsx
+++ packages/react-frontend/src/pages/HomePage.jsx
@@ -3,17 +3,25 @@ import PageContainer from "../components/PageContainer";
 import useIsMobile from "../hooks/useIsMobile";
 
 function HomePage() {
-    const isMobile = useIsMobile();
+  const isMobile = useIsMobile();
 
-    return (
-        <Layout>
-            <PageContainer>
-                <div className="home-page">
-                    <h1 className = "home-title">Welcome to Before or After!</h1>
-                </div>
-            </PageContainer>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <PageContainer>
+        <div className="home-page">
+          <img className="home-logo"
+            src={isMobile ? "https://openclipart.org/image/2000px/232064" : "https://openclipart.org/image/2000px/232064"}>
+          </img>
+          <h1 className="home-title">Welcome to Before or After!</h1>
+          <h2 className="home-tagline">
+            A daily game where players guess the release year of various
+            cultural artifacts
+          </h2>
+          <button className="play-button">Play</button>
+        </div>
+      </PageContainer>
+    </Layout>
+  );
 }
 
 export default HomePage;
