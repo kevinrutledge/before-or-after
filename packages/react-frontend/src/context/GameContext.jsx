32405f2d075717e12b3838eb0de9b350999bf8d6
ThomHags611 <thomashagos611@gmail.com>
2025-05-15 16:21:08 -0700

    feat: added state transitions, high score now saved locally

--- packages/react-frontend/src/context/GameContext.jsx
+++ packages/react-frontend/src/context/GameContext.jsx
@@ -1,11 +1,47 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const GameContext = createContext();
 
 export function GameProvider({ children }) {
   const [score, setScore] = useState(0);
-  const [highscore, setHighscore] = useState(0);
-  const [gameStatus, setGameStatus] = useState("not-started"); // or "playing", "lost", etc.
+  //sets highscore to 0 if nothing is found locally
+  const [highscore, setHighscore] = useState(() => {
+    const storedHighScore = localStorage.getItem("highscore");
+    //if saved high score is gound
+    if (storedHighScore !== null){
+      return parseInt(storedHighScore, 10);
+    }
+    //if no saved high score is found
+    else{
+      return 0
+    }});
+
+  
+  const [gameStatus, setGameStatus] = useState("initial"); // 'initial', 'playing', 'correct' 'incorrect' 'reshuffling'
+
+  
+  //anytime highscore is changed, save new value locally
+  useEffect(() => {
+    localStorage.setItem("highscore", highscore.toString());
+  }, [highscore])
+
+
+//increments score and changes game status
+const handleCorrectGuess = () => {
+  setScore(prev => prev + 1);
+  setGameStatus("correct");
+};
+
+//updates highscore and changes game status
+const handleIncorrectGuess = () =>{
+  //update high score
+  if(score > highscore){
+    setHighscore(score)
+  }
+
+  setGameStatus('incorrect');
+}
+
 
   return (
     <GameContext.Provider
@@ -15,12 +51,16 @@ export function GameProvider({ children }) {
         highscore,
         setHighscore,
         gameStatus,
-        setGameStatus
+        setGameStatus, 
+        handleCorrectGuess,
+        handleIncorrectGuess,
       }}>
       {children}
     </GameContext.Provider>
   );
 }
+
+
 
 export function useGame() {
   return useContext(GameContext);
