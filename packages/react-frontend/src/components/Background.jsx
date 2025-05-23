55c4eca6101b69bb72cc2ee03cdf83b3483c2384
Shonueld <seangriffindev@gmail.com>
2025-05-23 10:36:29 -0700

    fix

--- packages/react-frontend/src/components/Background.jsx
+++ packages/react-frontend/src/components/Background.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Background = () => {
   return (
@@ -6,7 +6,7 @@ const Background = () => {
       <div className="background" />
     </StyledWrapper>
   );
-};
+}
 
 const StyledWrapper = styled.div`
   position: fixed;
@@ -16,37 +16,13 @@ const StyledWrapper = styled.div`
   z-index: -1;
 
   .background {
-    width: 100vw;
-    height: 100vh;
-    --color: #e1e1e1;
-    background-color: #f3f3f3;
-    background-image:
-      linear-gradient(
-        0deg,
-        transparent 24%,
-        var(--color) 25%,
-        var(--color) 26%,
-        transparent 27%,
-        transparent 74%,
-        var(--color) 75%,
-        var(--color) 76%,
-        transparent 77%,
-        transparent
-      ),
-      linear-gradient(
-        90deg,
-        transparent 24%,
-        var(--color) 25%,
-        var(--color) 26%,
-        transparent 27%,
-        transparent 74%,
-        var(--color) 75%,
-        var(--color) 76%,
-        transparent 77%,
-        transparent
-      );
+    width: 100%;
+    height: 100%;
+    --color: #E1E1E1;
+    background-color: #F3F3F3;
+    background-image: linear-gradient(0deg, transparent 24%, var(--color) 25%, var(--color) 26%, transparent 27%,transparent 74%, var(--color) 75%, var(--color) 76%, transparent 77%,transparent),
+        linear-gradient(90deg, transparent 24%, var(--color) 25%, var(--color) 26%, transparent 27%,transparent 74%, var(--color) 75%, var(--color) 76%, transparent 77%,transparent);
     background-size: 55px 55px;
-  }
-`;
+  }`;
 
 export default Background;
