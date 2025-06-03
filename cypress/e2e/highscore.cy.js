1e80ca89049cc5f5b1924d1db15728ddeced39e4
ThomHags611 <thomashagos611@gmail.com>
2025-06-03 13:18:05 -0700

    test: more test cases

--- cypress/e2e/highscore.cy.js
+++ cypress/e2e/highscore.cy.js
@@ -1,11 +1,15 @@
-// function userLogin() {
-//   cy.visit('http://localhost:5173/login');
-//   cy.get('#emailOrUsername').type('testuser@example.com'); 
-//   cy.get('input[type="password"]').type('password123');
-//   cy.contains('button', 'Sign In').click();
-//   cy.contains('A daily game').should('be.visible');
-//   cy.contains('Play').click();
-//   cy.contains('Current Score: 0').should('be.visible');
+// function userSignUp() {
+//   cy.visit('http://localhost:5173/signup');
+//   cy.get('#email').type('testuser2@example.com'); 
+//   cy.get('#username').type('testuser2'); 
+//   cy.get('#password').type('password123');
+//   cy.get('#confirmPassword').type('password123');
+//   cy.contains('button', 'Sign Up').click();
+//   //cy.contains('A daily game').should('be.visible');
+//   //cy.contains('Play').click();
+//   //cy.contains('Current Score: 0').should('be.visible');
+//   cy.wait(1000); // wait 1000ms (1 second)
+
 // }
 
 // function guestLogin(){
@@ -74,13 +78,6 @@
 
 // describe('Authentication and Game Play', () => {
 //     it('user can not navigate to admin page', () => {
-//         userLogin();
-//         cy.visit('http://localhost:5173/admin');
-//         cy.url().should('not.include', '/admin');
-//     });
-//     it('guest cant navigate to admin page', () => {
-//         guestLogin();
-//         cy.visit('http://localhost:5173/admin');
-//         cy.url().should('not.include', '/admin');
+//         userSignUp();
 //     });
 // });
